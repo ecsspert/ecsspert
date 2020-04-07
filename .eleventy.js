31eb496bf7aff8609e2885f906d4003fa8ccbe97
zoreet <zoreetdotcom@gmail.com>
2020-04-07 23:52:24 +0200

    got adidas to look ok

---
+++ .eleventy.js
@@ -0,0 +1,43 @@
+const Prism = require('prismjs');
+Prism.plugins.LineNumbers = true;
+const beautifyHTML = require('js-beautify').html;
+const beautifyCSS = require('js-beautify').css;
+
+const beautifySettings = {
+  "indent_size": "2",
+  "indent_char": " ",
+  "end_with_newline": false,
+};
+
+addLineNumbers = function(code) {
+  let lineNumbers = '';
+  for(let i=1; i<=100; i++) {
+    lineNumbers += `<span>${i}</span>`
+  }
+  return `${code}<span class='line-numbers'>${lineNumbers}</span>`;
+}
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/images')
+
+  eleventyConfig.addFilter("htmlCode", function(value) {
+    let code = beautifyHTML(value, beautifySettings)
+    code = Prism.highlight(code, Prism.languages.html, 'html');
+    code = addLineNumbers(code);
+    return code;
+  });
+
+  eleventyConfig.addFilter("cssCode", function(value) {
+    let code = beautifyCSS(value, beautifySettings)
+    code = Prism.highlight(code, Prism.languages.css, 'css');
+    code = addLineNumbers(code);
+    return code;
+  });
+
+  return {
+    dir: { input: 'src', output: 'dist', data: '_data' },
+    passthroughFileCopy: true,
+    templateFormats: ['njk', 'md', 'css', 'html', 'yml'],
+    htmlTemplateEngine: 'njk'
+  }
+}
